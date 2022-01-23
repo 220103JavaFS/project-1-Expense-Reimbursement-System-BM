04333a6a9ff88a1a76be9bba4cbf902c07e93551
rfloyd01 <lynrdskynrd05@gmail.com>
2022-01-23 09:44:21 -0500

    configured app to load static HTML pages from inside application

--- src/test/java/com/revature/App.java
+++ src/test/java/com/revature/App.java
@@ -1,15 +1,19 @@
 package com.revature;
 
 import com.revature.controllers.Controller;
+import com.revature.controllers.TestController;
 import io.javalin.Javalin;
+import io.javalin.http.staticfiles.Location;
 
 public class App {
 
     private static Javalin app;
 
     public static void main(String[] args) {
-        app = Javalin.create();
-        configure();
+        app = Javalin.create((config)->{
+            config.addStaticFiles("/web", Location.CLASSPATH);
+        });
+        configure(new TestController());
         app.start();
     }
 
